5bb3ed7b5852890d28968f68936a00a84b6b571a
debrupasen <81070289+debrupasen@users.noreply.github.com>
2021-03-22 15:48:18 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,17 @@
+#add implementation
+def add (x,y):
+  return x+y
+#subtraction implementation
+def subtract(x,y):
+   return x-y
+ #multiply implementation
+def multiply(x,y):
+ return x*y
+  # pass
+#divide implementation
+def divide(x,y):
+ return x/y
+ #  pass
+ #sq
+ def square (x) :
+pass
